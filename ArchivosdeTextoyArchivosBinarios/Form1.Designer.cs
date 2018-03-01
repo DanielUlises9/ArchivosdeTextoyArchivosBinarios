882a45669252817276df07a136466b531ae0e44c
DanielUlises9 <dmendoza7@ucol.mx>
2018-02-28 20:31:56 -0600

    bitmap y xml
    leector de archivos bitma py creador de archivos xml

--- ArchivosdeTextoyArchivosBinarios/Form1.Designer.cs
+++ ArchivosdeTextoyArchivosBinarios/Form1.Designer.cs
@@ -28,12 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtFactName = new System.Windows.Forms.TextBox();
+            this.txtFactPet = new System.Windows.Forms.TextBox();
+            this.txtFactColor = new System.Windows.Forms.TextBox();
+            this.txtFactAddress = new System.Windows.Forms.TextBox();
+            this.txtFactNumber = new System.Windows.Forms.TextBox();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.sfdWindowSave = new System.Windows.Forms.SaveFileDialog();
+            this.txtResultadobmp = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ofdBitmap = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtFactName
+            // 
+            this.txtFactName.Location = new System.Drawing.Point(122, 129);
+            this.txtFactName.Name = "txtFactName";
+            this.txtFactName.Size = new System.Drawing.Size(100, 20);
+            this.txtFactName.TabIndex = 0;
+            // 
+            // txtFactPet
+            // 
+            this.txtFactPet.Location = new System.Drawing.Point(122, 240);
+            this.txtFactPet.Name = "txtFactPet";
+            this.txtFactPet.Size = new System.Drawing.Size(100, 20);
+            this.txtFactPet.TabIndex = 1;
+            // 
+            // txtFactColor
+            // 
+            this.txtFactColor.Location = new System.Drawing.Point(122, 214);
+            this.txtFactColor.Name = "txtFactColor";
+            this.txtFactColor.Size = new System.Drawing.Size(100, 20);
+            this.txtFactColor.TabIndex = 2;
+            // 
+            // txtFactAddress
+            // 
+            this.txtFactAddress.Location = new System.Drawing.Point(122, 188);
+            this.txtFactAddress.Name = "txtFactAddress";
+            this.txtFactAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtFactAddress.TabIndex = 3;
+            // 
+            // txtFactNumber
+            // 
+            this.txtFactNumber.Location = new System.Drawing.Point(122, 162);
+            this.txtFactNumber.Name = "txtFactNumber";
+            this.txtFactNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtFactNumber.TabIndex = 4;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(260, 136);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 5;
+            this.btnCrear.Text = "Crear XML";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Numero de Telefono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Direccion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Color Favorito";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Mascota Favorita";
+            // 
+            // txtResultadobmp
+            // 
+            this.txtResultadobmp.Location = new System.Drawing.Point(149, 12);
+            this.txtResultadobmp.Multiline = true;
+            this.txtResultadobmp.Name = "txtResultadobmp";
+            this.txtResultadobmp.Size = new System.Drawing.Size(244, 78);
+            this.txtResultadobmp.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Abrir bit map";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ofdBitmap
+            // 
+            this.ofdBitmap.FileName = "ofdBitmap";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 341);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtResultadobmp);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.txtFactNumber);
+            this.Controls.Add(this.txtFactAddress);
+            this.Controls.Add(this.txtFactColor);
+            this.Controls.Add(this.txtFactPet);
+            this.Controls.Add(this.txtFactName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtFactName;
+        private System.Windows.Forms.TextBox txtFactPet;
+        private System.Windows.Forms.TextBox txtFactColor;
+        private System.Windows.Forms.TextBox txtFactAddress;
+        private System.Windows.Forms.TextBox txtFactNumber;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.SaveFileDialog sfdWindowSave;
+        private System.Windows.Forms.TextBox txtResultadobmp;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog ofdBitmap;
     }
 }
 
--- ArchivosdeTextoyArchivosBinarios/Form1.cs
+++ ArchivosdeTextoyArchivosBinarios/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,88 @@ namespace ArchivosdeTextoyArchivosBinarios
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            XmlDocument();
+        }
+
+        void XmlDocument ()
+        {
+            sfdWindowSave.Filter = "XML-File|*.xml";
+            sfdWindowSave.ShowDialog();
+            
+            FileStream fs = new FileStream(sfdWindowSave.FileName, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            
+            string datos= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\r"
+                           +"<Datos>"
+                           +"<name>"+txtFactName.Text+"</name>"
+                           +"<number>"+txtFactNumber.Text+"</number>"
+                           +"<address>"+txtFactAddress.Text+"</address>"
+                           +"<color>"+txtFactColor.Text+"</color>"
+                           +"<pet>"+txtFactPet.Text+"</pet>"
+                           +"</Datos>";
+            sw.Write (datos);
+
+            sw.Close();
+            fs.Close();
+            txtFactAddress.Text = "";
+            txtFactName.Text = "";
+            txtFactPet.Text = "";
+            txtFactNumber.Text = "";
+            txtFactColor.Text = "";
+            MessageBox.Show("El archivo XML a sido creado");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BitMap();
+        }
+
+        void BitMap()
+        {
+            txtResultadobmp.Text = "";
+            ofdBitmap.ShowDialog();
+            
+            FileStream fs = 
+                new FileStream(ofdBitmap.FileName,FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+
+            char b = br.ReadChar();
+            char m = br.ReadChar();
+
+            if (b != 60 && m != 77)
+            {
+                MessageBox.Show("El tipo de formato no es .bmp");
+                br.Close();
+                fs.Close();
+            }
+            else
+            {
+                int size = br.ReadInt32();
+                ulong trash = br.ReadUInt64();
+                int trash2 = br.ReadInt32();
+
+                int width = br.ReadInt32();
+                int heigth = br.ReadInt32();
+
+                int trash3 = br.ReadInt16();
+
+                int pixelsize = br.ReadInt16();
+
+                txtResultadobmp.Text = "Tamño del bit map {"+size.ToString()+"}\r\nTamaño de la anchura {"+width.ToString()+ "}\r\nTamaño de la altura {" + heigth.ToString()+ "}\r\ntotal de piexeles en la imagen {" + pixelsize.ToString()+"}";
+                br.Close();
+                fs.Close();
+
+            }
+
         }
     }
 }
